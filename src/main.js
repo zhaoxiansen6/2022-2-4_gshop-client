3c21d5d453c42e83f1fbe13325341c4ac1629512
zwx <2425180604@qq.com>
2022-02-04 23:21:22 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+//浏览器控制台不显示非生产环境打包的提示
 Vue.config.productionTip = false
 
 new Vue({
---
+++ vue.config.js
@@ -0,0 +1,5 @@
+// 关闭规则检查
+module.exports = {
+  lintOnSave:false, //关闭 ESlint 的规则检查
+  lintOnSave:'warning' //输出提示错误,但项目继续进行
+}
